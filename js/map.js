95d0cd3e147e2063ced4031b7f805d39a1035f38
Rigel J <rja387@nyu.edu>
2018-03-07 14:04:59 -0500

    nav, tabs, deaths
    style changes to modals
    ad start modal
    ad number of deaths

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ $(function () {
 })
 
 $(window).on('load',function(){
-    $('#AboutModalLong').modal('show');
+    $('#StartModalLong').modal('show');
 });
 
 
@@ -23,8 +23,11 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z
 // Styles
 
 var DamageColorArray 		= ['#FF8C00','#FFD700','#DC143C','#DC143C','#00FFFFFF'];
-// var SeismicArray 				= ['#FF0000','#FFA07A','#006400','#66CDAA','#FFFF00','#00FFFFFF'];
-var OpacityArray				= ['0','1','0.8','0.3']
+// var SeismicArray 		= ['#FF0000','#FFA07A','#006400','#66CDAA','#FFFF00','#00FFFFFF'];
+var OpacityArray				= ['0',       '1',      '0.8',    '0.3']
+var radiusArray         = [25,         20,       15,       10,       5];
+var TotDeathArray       = [30,         20,       10,        5,       1];
+
 
 const getColor = function(s) {
   switch(s) {
@@ -67,6 +70,26 @@ function Style(feature) {
   };
 }
 
+function getRadius(deaths) {
+  return deaths = null ? 0:
+         deaths >TotDeathArray[0] ? radiusArray[0] :
+         deaths >TotDeathArray[1] ? radiusArray[1] :
+         deaths >TotDeathArray[2] ? radiusArray[2] :
+         deaths >TotDeathArray[3] ? radiusArray[3] :
+         deaths >TotDeathArray[4] ? radiusArray[4] :
+              radiusArray[5] ;
+}
+
+function deathStyle(feature) {
+  return {
+    weight: 0,
+    opacity: 0.8,
+    fillOpacity: 0.6,
+    fillColor: '#8B0000',
+    radius: getRadius(feature.properties.descriptio)
+  };
+}
+
 // function zoneStyle(feature) {
 //   return {
 // 		weight: 0,
@@ -76,10 +99,13 @@ function Style(feature) {
 //   };
 // }
 
-// function onEachFeature (feature,layer) {
-//   var popup=layer.bindPopup(`
-//     <b style='font-size: 120%'> getType(${feature.properties.hour})</b><br/>`)
-// }
+function onEachFeature (feature,layer) {
+  var popup=layer.bindPopup(`
+    <b style='font-size: 120%'>Number of deaths:</b> ${feature.properties.descriptio}<br/>
+    <b style='font-size: 120%'>Discrepancy on number of deaths:</b> ${feature.properties.dato_contr}<br/>
+    <b style='font-size: 120%'>Location :</b> ${feature.properties.name}
+  `)
+}
 
 var mexicostyle = {
 									opacity: OpacityArray[1],
@@ -125,4 +151,13 @@ var damages = L.geoJson(atlas, {
   },
   style: Style,
   // onEachFeature: onEachFeature
+})
+.addTo(map)
+
+var deaths = L.geoJson(fallecidos, {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng)
+  },
+  style: deathStyle,
+  onEachFeature: onEachFeature
 }).addTo(map)
