ddf7cb38a5b6e794ea2a9020ae2a3471ce60e2ea
Rigel J <rja387@nyu.edu>
2019-02-01 16:47:57 +0000

    other changes

--- js/map.js
+++ js/map.js
@@ -22,68 +22,72 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z
 
 // Styles
 
-var DamageColorArray 		= ['#FF8C00','#FFD700','#DC143C','#DC143C','#00FFFFFF'];
+// var DamageColorArray 		= ['#FF8C00','#FFD700','#DC143C','#DC143C','#00FFFFFF'];
+var CollapseColorArray 	= ['#808080','#808080','#C0C0C0','#C0C0C0','#00FFFFFF'];
+
 // var SeismicArray 		= ['#FF0000','#FFA07A','#006400','#66CDAA','#FFFF00','#00FFFFFF'];
 var OpacityArray				= ['0',       '1',      '0.8',    '0.3']
-var radiusArray         = [25,         20,       15,       10,       5];
+var radiusArray         = [30,         20,       15,       10,       5];
 var TotDeathArray       = [30,         20,       10,        5,       1];
 
-
-const getColor = function(s) {
-  switch(s) {
-    case 0 :   return {color: DamageColorArray[0], opacity: OpacityArray[2]};
-    case 1 :   return {color: DamageColorArray[1], opacity: OpacityArray[2]};
-    case 2 :   return {color: DamageColorArray[2], opacity: OpacityArray[2]};
-    case 3 :   return {color: DamageColorArray[3], opacity: OpacityArray[2]};
-    case 4 :   return {color: DamageColorArray[4], opacity: OpacityArray[0]};
-  }
-}
-
-// const getSeismic = function(s) {
+// Style for damaged buildings:
+//
+// const DamagegetColor = function(s) {
 //   switch(s) {
-//     case 0 :   return {color: SeismicArray[0], opacity: OpacityArray[2]};
-//     case 1 :   return {color: SeismicArray[1], opacity: OpacityArray[2]};
-//     case 2 :   return {color: SeismicArray[2], opacity: OpacityArray[2]};
-//     case 3 :   return {color: SeismicArray[3], opacity: OpacityArray[2]};
-//     case 4 :   return {color: SeismicArray[4], opacity: OpacityArray[2]};
-//     case 5 :   return {color: SeismicArray[5], opacity: OpacityArray[0]};
+//     case 0 :   return {color: DamageColorArray[0], opacity: OpacityArray[2]};
+//     case 1 :   return {color: DamageColorArray[1], opacity: OpacityArray[2]};
+//     case 2 :   return {color: DamageColorArray[2], opacity: OpacityArray[2]};
+//     case 3 :   return {color: DamageColorArray[3], opacity: OpacityArray[2]};
+//     case 4 :   return {color: DamageColorArray[4], opacity: OpacityArray[0]};
 //   }
 // }
+//
+// function Style(feature) {
+//   return {
+//     weight: 0,
+//     opacity: DamagegetColor(feature.properties.SymbolID).opacity,
+//     fillOpacity: DamagegetColor(feature.properties.SymbolID).opacity,
+//     fillColor: DamagegetColor(feature.properties.SymbolID).color,
+//     radius: 5
+//   };
+// }
 
-const getType = function(s) {
+// Style for collapsed buildings:
+
+const getColorCollapse = function(s) {
   switch(s) {
-    case 0 :   return 'Major Damage';
-    case 1 :   return 'Some Damage';
-    case 2 :   return 'Collapse';
-    case 3 :   return 'Collapse';
-    case 4 :   return 'Others';
+    case 0 :   return {color: CollapseColorArray[0], opacity: OpacityArray[0]};
+    case 1 :   return {color: CollapseColorArray[1], opacity: OpacityArray[0]};
+    case 2 :   return {color: CollapseColorArray[2], opacity: OpacityArray[2]}; // Only collapsed buildings show
+    case 3 :   return {color: CollapseColorArray[3], opacity: OpacityArray[2]}; // Only collapsed buildings show
+    case 4 :   return {color: CollapseColorArray[4], opacity: OpacityArray[0]};
   }
 }
 
-function Style(feature) {
+function CollapseStyle(feature) {
   return {
     weight: 0,
-    opacity: getColor(feature.properties.SymbolID).opacity,
-    fillOpacity: getColor(feature.properties.SymbolID).opacity,
-    fillColor: getColor(feature.properties.SymbolID).color,
-    radius: 5
+    opacity: getColorCollapse(feature.properties.SymbolID).opacity,
+    fillOpacity: getColorCollapse(feature.properties.SymbolID).opacity,
+    fillColor: getColorCollapse(feature.properties.SymbolID).color,
+    radius: 3
   };
 }
 
-function getRadius(deaths) {
-  return deaths = null ? 0:
-         deaths >TotDeathArray[0] ? radiusArray[0] :
-         deaths >TotDeathArray[1] ? radiusArray[1] :
-         deaths >TotDeathArray[2] ? radiusArray[2] :
-         deaths >TotDeathArray[3] ? radiusArray[3] :
-         deaths >TotDeathArray[4] ? radiusArray[4] :
+function getRadius(victims) {
+  return victims = null ? 0:
+         victims >TotDeathArray[0] ? radiusArray[0] :
+         victims >TotDeathArray[1] ? radiusArray[1] :
+         victims >TotDeathArray[2] ? radiusArray[2] :
+         victims >TotDeathArray[3] ? radiusArray[3] :
+         victims >TotDeathArray[4] ? radiusArray[4] :
               radiusArray[5] ;
 }
 
-function deathStyle(feature) {
+function victimStyle(feature) {
   return {
     weight: 0,
-    opacity: 0.8,
+    opacity: 0.6,
     fillOpacity: 0.6,
     fillColor: '#8B0000',
     radius: getRadius(feature.properties.descriptio)
@@ -101,8 +105,8 @@ function deathStyle(feature) {
 
 function onEachFeature (feature,layer) {
   var popup=layer.bindPopup(`
-    <b style='font-size: 120%'>Number of deaths:</b> ${feature.properties.descriptio}<br/>
-    <b style='font-size: 120%'>Discrepancy on number of deaths:</b> ${feature.properties.dato_contr}<br/>
+    <b style='font-size: 120%'>Number of victims:</b> ${feature.properties.descriptio}<br/>
+    <b style='font-size: 120%'>Discrepancy: </b> ${feature.properties.dato_contr}<br/>
     <b style='font-size: 120%'>Location :</b> ${feature.properties.name}
   `)
 }
@@ -121,7 +125,6 @@ var lakestyle = {
 									fillOpacity: OpacityArray[3],
 }
 
-
 // Mexico City Boundary
 var mexicocity = L.geoJson(cdmx, {
   style: mexicostyle
@@ -132,32 +135,30 @@ var texcocolake = L.geoJson(lake, {
   style: lakestyle
 }).addTo(map)
 
-// // zone
-// var seismiczoning = L.geoJson(zone, {
-//   style: zoneStyle
+// // Damages
+// var damages = L.geoJson(atlas, {
+//   pointToLayer: function (feature, latlng) {
+//     return L.circleMarker(latlng)
+//   },
+//   style: Style,
+//   // onEachFeature: onEachFeature
 // })
-// // LAYER CONTROL
-// var overlay = {
-// 	'Lake Texcoco': texcocolake,
-// 	'Seismic Zoning': seismiczoning,
-// };
-//
-// L.control.layers(overlay,null,{collapsed:false}).addTo(map);
+// // .addTo(map)
 
-// Damages
+// collapsed buildings
 var damages = L.geoJson(atlas, {
   pointToLayer: function (feature, latlng) {
     return L.circleMarker(latlng)
   },
-  style: Style,
-  // onEachFeature: onEachFeature
+  style: CollapseStyle,
 })
 .addTo(map)
 
-var deaths = L.geoJson(fallecidos, {
+// Victims
+var victims = L.geoJson(fallecidos, {
   pointToLayer: function (feature, latlng) {
     return L.circleMarker(latlng)
   },
-  style: deathStyle,
+  style: victimStyle,
   onEachFeature: onEachFeature
 }).addTo(map)
